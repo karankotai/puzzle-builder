8873943921e039f6f26d709dbec415cd6123afed
Karan kotai <karankotai9183@gmail.com>
2025-07-03 16:55:09 +0530

    Update LogicPuzzle.tsx

--- src/components/LogicPuzzle.tsx
+++ src/components/LogicPuzzle.tsx
@@ -53,7 +53,7 @@ const LogicPuzzle = ({ latest }: { latest: boolean }) => {
     const fetchData = async () => {
       setLoading(true)
       try {
-        const response = await axios.get(`https://tryingre.onrender.com/puzzles/allPuzzle`)
+        const response = await axios.get(`https://tryingre.onrender.com/puzzles`)
         const puzzles = await response?.data?.Puzzles;
         if (!puzzles || puzzles.length == 0) {
           setLoading(false)
